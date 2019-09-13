536dda181ee1bb935df4ef60cb3442796c5389f4
cwern40 <chris.wernli@hotmail.com>
2019-09-12 18:51:10 -0600

    I created the endpoints for get all, add, and remove

---
+++ starwars/characters-model.js
@@ -0,0 +1,17 @@
+module.exports = {
+    find,
+    add,
+    remove
+}
+
+function find() {
+
+}
+
+function add() {
+
+}
+
+function remove() {
+    
+}
---
+++ starwars/characters-route.js
@@ -0,0 +1,41 @@
+const router = require('express').Router()
+
+const Characters = require('./characters-model');
+
+router.get('/', (req, res) => {
+    Characters.find()
+        .then(characters => {
+            res.json(characters)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
+router.post('/', (req, res) => {
+    const characterData = req.body;
+
+    Characters.add(characterData)
+        .then(saved => {
+            res.status(201).json(saved)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+
+    Characters.remove(id)
+        .then(removed => {
+            res.json({
+                message: "character successfully removed"
+            })
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
+module.exports = router;
